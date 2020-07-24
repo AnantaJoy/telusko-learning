6cd8517dcb30eb44248f5077ac133a23bb00a697
Ananta Asim Joy <anantajoy007@gmail.com>
2020-07-24 12:47:31 +0600

    javascript files added

---
+++ javaScript/02_variables,data_types.js
@@ -0,0 +1,9 @@
+// data types in javascript
+var number1 = 10;
+var message = "Oh! I've learn something of javascript"
+var booleanValue = true;
+
+console.log(typeof(number1));
+console.log(typeof(message));
+console.log(typeof(booleanValue));
+console.log(typeof(19.9));
---
+++ javaScript/03_operators.js
@@ -0,0 +1,40 @@
+var x = 10;
+var y = 8;
+
+// let's do some math
+console.log('Basic Mathematics Operation:');
+console.log('10 + 8 =',x+y);
+console.log('10 - 8 =',x-y);
+console.log('10 * 8 =',x*y);
+console.log('10 / 8 =',x/y);
+
+
+console.log(x%y);
+console.log('10 is equal to 8. Ans:', x==y);
+console.log('10 is not equal to 8. Ans.',x!=y);
+console.log(10**3);
+
+console.log(x++);
+console.log(++x);
+console.log(--y)
+console.log(y--);
+
+console.log('Hello,'+'World!');
+console.log('Hello,','World!');
+
+console.log('I\'m'+21+'Years Old');
+console.log('I\'m',21,'Years Old');
+console.log(5+7);
+console.log('5'+5);
+console.log(4+5+'Hello'+4+5);
+
+
+// equal type and equal value ===
+// equal type and not equal value !==
+// greater than >
+// less than <
+// greater than or equal to >=
+// less than or equal to <=
+// ternary operator ?:condition1:condition2
+// logical operator or, and , not, xor, right shift, left shift
+// Order of Precedence
