10ec45bab67fb7a5efb93d013916abe6ab79742b
Ananta Asim Joy <anantajoy007@gmail.com>
2020-09-23 16:40:32 +0000

    different algorithms

---
+++ python/Sorting/bubble_sort.py
@@ -0,0 +1,13 @@
+def sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+           if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+
+nums = [2,3,6,84,1,46,35,29]
+
+sort(nums)
+print(nums)
---
+++ python/Sorting/selection_sort.py
@@ -0,0 +1,6 @@
+
+
+nums = [2,3,6,84,1,46,35,29]
+
+sort(nums)
+print(nums)
